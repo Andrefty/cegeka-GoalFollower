26b4ee376a78523a5b1d15005d9d921ae132da55
DomnulCiot <cristiciotei@gmail.com>
2018-06-04 22:03:46 +0300

    how to train your dragon

--- GoalFollower/app/src/main/java/cegeka/goalfollower/ro/goalfollower/ListActivity.java
+++ GoalFollower/app/src/main/java/cegeka/goalfollower/ro/goalfollower/ListActivity.java
@@ -1,7 +1,11 @@
 package cegeka.goalfollower.ro.goalfollower;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.FileInputStream;
@@ -10,11 +14,20 @@ import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
     ArrayList<Goal> returnlist=null;
-
+    String[] names;
+    String[] duedate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+     //  Readf();
+        ListView GoalListView = (ListView) findViewById(R.id.GoalListView);
+        Resources res = getResources();
+        names = res.getStringArray(R.array.names);
+        duedate = res.getStringArray(R.array.due_date);
+        com.example.cristi.firstcegeka.ItemAdapter itemAdapt =  new com.example.cristi.firstcegeka.ItemAdapter(this , names , duedate );
+        GoalListView.setAdapter(itemAdapt);
+
 
     }
   public void Readf(){FileInputStream fis;
